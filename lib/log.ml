67dd6887bfa6b052caab7c9d88568dbf1f4eadf8
theteachr <sop.theteachr@gmail.com>
2023-11-15 18:17:10 +0530

    :sparkles: Clean branches

--- lib/log.ml
+++ lib/log.ml
@@ -1,36 +1,32 @@
-let ( let* ) = Option.bind
-
 type timestamp = int
 
 type t = {
-  message : string;
   level : Log_level.t;
   timestamp : timestamp;
+  message : string;
 }
 
-let _make ~message ~level ~timestamp = { message; level; timestamp }
+let make level timestamp message = { level; timestamp; message }
+
+let info = make Info
+
+let warn = make Warn
+
+let error severity timestamp message =
+  { level = Error severity; timestamp; message }
 
 let parse line =
-  let use_line line = Scanf.sscanf line in
-  let difficult _ line =
-      use_line line "%_c %d %d %[^\n]" (fun severity timestamp message ->
-          { level = Log_level.Error severity; timestamp; message })
-      |> Option.some
+  let read_log format receiver line =
+    try Some (Scanf.sscanf line format receiver) with _ -> None
   in
-  let simple level line =
-    let* level = level in
-    Scanf.sscanf line "%_c %d %[^\n]" (fun timestamp message ->
-        { level; timestamp; message })
-    |> Option.some
-  in
-  let level, f =
+  let scan =
     match String.get line 0 with
-    | 'I' -> (Some Log_level.Info, simple)
-    | 'W' -> (Some Log_level.Warn, simple)
-    | 'E' -> (None, difficult)
-    | _ -> (None, fun _ _ -> None)
+    | 'I' -> read_log "%_c %d %[^\n]" info
+    | 'W' -> read_log "%_c %d %[^\n]" warn
+    | 'E' -> read_log "%_c %d %d %[^\n]" error
+    | _ -> fun _ -> None
   in
-  f level line
+  scan line
 
-let to_string { timestamp; level; message } =
+let to_string { level; timestamp; message } =
   Printf.sprintf "[%d] [%s] %s" timestamp (Log_level.to_string level) message
